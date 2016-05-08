52005531b8ceb83e4ccac116748a968a58861be3
bklynate <nathanielcfa@gmail.com>
2016-05-08 03:52:08 -0400

    add show route

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ mongoose.connect("mongodb://localhost/yelpcamp_db");
 // SCHEMA SET UP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //   name: "Lion Head",
-//   image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg"
+//   image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg",
+//   description: "Brave the wilderness of the beautiful 'Lion Head Preservation' "
 // },function(error,campground){
 //   if(error){
 //     console.log("Error: Something farted...");
@@ -28,7 +30,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(request, response){
-  response.render("landing")
+  response.render("landing");
 });
 
 app.get("/campgrounds", function(request, response){
@@ -36,22 +38,37 @@ app.get("/campgrounds", function(request, response){
     if(error){
       console.log(error);
     } else {
-      response.render("campgrounds", {campgrounds: campgrounds})
+      response.render("campgrounds", {campgrounds: campgrounds});
     }
   });
 });
 
 app.post("/campgrounds", function(request, response){
-  var campgroundName = request.body.campgroundName
-  var image = request.body.image
-  var newCampground = {name: campgroundName, image: image};
-  campgrounds.push(newCampground);
-  response.redirect("/campgrounds")
+  var campgroundName = request.body.campgroundName;
+  var image = request.body.image;
+  var newCampground = {
+    name: campgroundName,
+    image: image
+  };
+
+  Campground.create(newCampground, function(error, newCampground){
+    if(error){
+      console.log(error);
+    } else {
+      response.redirect("/campgrounds");
+    }
+  })
 });
 
 app.get("/campgrounds/new", function(request, response){
-  response.render("new")
+  response.render("new");
 });
+
+// SHOW - Show page for each campsite
+app.get("/campgrounds/:id", function(request, response){
+  var campground = Campground.find({})
+  response.render("show");
+})
 
 // App Begins Listening Here
 app.listen(3000, function(){
