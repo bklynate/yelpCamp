ea477e9c9bfe4fc7d3d76a8898dfa8beaf328306
bklynate <nathanielcfa@gmail.com>
2016-05-09 19:31:30 -0400

    heroku deployment

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var express = require("express"),
     mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost/yelpcamp_db");
-// mongoose.connect("mongodb://nathan:bloods12@ds017432.mlab.com:17432/heroku_0x90k9m5")
-
 
 // SCHEMA SET UP
 var campgroundSchema = new mongoose.Schema({
