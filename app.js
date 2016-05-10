9d9d478cbd07cde89b27c90cc77f33be238a92db
bklynate <nathanielcfa@gmail.com>
2016-05-09 20:37:50 -0400

    heroku deployment

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ Campground.create({
   }
 });
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
