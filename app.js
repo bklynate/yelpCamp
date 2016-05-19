9349462683c88502e504aa6667b7cf6aba2e1772
bklynate <nathanielcfa@gmail.com>
2016-05-18 20:04:42 -0400

    add nested routes for comments, comment model, appjs fixes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
-    seedDB = require("./seeds")
+    seedDB = require("./seeds"),
+    Comment = require("./models/comment")
 
 // mongoose.connect(process.env.DATABASEURL);
 mongoose.connect("mongodb://localhost/yelpcamp_db_v3");
@@ -13,7 +14,7 @@ app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-seedDB();
+// seedDB();
 
 app.get("/", function(request, response){
   response.render("landing");
@@ -24,7 +25,7 @@ app.get("/campgrounds", function(request, response){
     if(error){
       console.log(error);
     } else {
-      response.render("index", {campgrounds: campgrounds});
+      response.render("campgrounds/index", {campgrounds: campgrounds});
     }
   });
 });
@@ -45,28 +46,60 @@ app.post("/campgrounds", function(request, response){
     } else {
       response.redirect("/campgrounds");
     }
-  })
+  });
 });
 
 app.get("/campgrounds/new", function(request, response){
-  response.render("new");
+  response.render("campgrounds/new");
 });
 
 // SHOW - Show page for each campsite
 app.get("/campgrounds/:id", function(request, response){
   var id = request.params.id
-  var campground = Campground.findById(id).populate("comments").exec(function(error, foundCamp){
+  Campground.findById(id).populate("comments").exec(function(error, foundCamp){
     if(error){
-      console.log(error)
+      console.log(error);
     } else {
-      console.log(foundCamp);
-      response.render("show",{ campground: foundCamp });
+      response.render("campgrounds/show", { campground: foundCamp });
+    }
+  });
+});
+
+//============================
+// COMMENTS ROUTES
+//============================
+app.get("/campgrounds/:id/comments/new", function(request, response){
+  Campground.findById(request.params.id, function(error,campground){
+    if(error){
+      console.log(error);
+    } else {
+      response.render("comments/new", { campground: campground});
     }
   })
-})
+});
+
+app.post("/campgrounds/:id/comments", function(request, response){
+  // Find the correct campground to leave the comment on
+  Campground.findById(request.params.id, function(error, campground){
+    if(error){
+      console.log(error);
+    } else {
+      // Creates the new comment
+      Comment.create(request.body.comment, function(error, comment){
+        if(error){
+          console.log(error);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          // redirect to the campground page
+          response.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  })
+});
 
 // App Begins Listening Here
-
 app.listen(app.get('port'), function(){
   console.log("Nathaniel made me listen...");
 });
